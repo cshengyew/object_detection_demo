87fad9b9fc55aadfc6bac631ebc14d3058fee892
cshengyew <46912414+cshengyew@users.noreply.github.com>
2020-01-21 14:56:58 +0800

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -7,7 +7,7 @@ python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FO
 python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOLDER]/test_labels.csv
 """
 
-mport os
+import os
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
